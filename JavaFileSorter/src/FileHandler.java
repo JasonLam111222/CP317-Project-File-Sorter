059d68f8a17ed6a4cebe074c617cad1b7fc920f5
JasonLam111222 <157567859+JasonLam111222@users.noreply.github.com>
2025-03-17 18:41:34 -0400

    Delete JavaFileSorter directory

--- JavaFileSorter/src/FileHandler.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-abstract class FileHandler {
-    public abstract List<Product> readData(String filePath) throws IOException;
-    public abstract void writeData(String filePath, List<Product> products) throws IOException;
-}
