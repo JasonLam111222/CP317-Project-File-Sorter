2899df3fdcd9670f917b90fd5ef61f7d0214a6d2
Mohid Elahi <124029998+Mohid44@users.noreply.github.com>
2025-03-17 18:46:57 -0400

    Update FileHandler.java

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,13 +1,25 @@
 import java.io.*;
 import java.util.*;
 
- public Supplier(int supplierID, String name, String address, String phone, String email) {
+class Supplier {
+    private int supplierID;
+    private String name, address, phone, email;
+
+    
+    public Supplier(int supplierID, String name, String address, String phone, String email) {
         this.supplierID = supplierID;
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.email = email;
     }
+
+    
+    public int getSupplierID() { return supplierID; }
+   
+    public String getName() { return name; }
+}
+
 abstract class FileHandler {
     public abstract List<Product> readData(String filePath) throws IOException;
     public abstract void writeData(String filePath, List<Product> products) throws IOException;
