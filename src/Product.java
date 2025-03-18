5131635aceb7fefa05e3fffde8b98f9e3c3865fa
Mohid Elahi <124029998+Mohid44@users.noreply.github.com>
2025-03-18 01:34:36 -0400

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,10 +1,26 @@
-class Product {
-    private String productId;
-    private String name;
-    private double price;
+package com.inventory;
 
-    public Product(String productId, String name, double price) {
-        setProductId(productId);
-        setName(name);
-        setPrice(price);
+public class Product {
+    private int productID;
+    private String name, description, status;
+    private double price;
+    private int quantity;
+    private Supplier supplier;
+
+    public Product(int productID, String name, String description, double price, int quantity, String status, Supplier supplier) {
+        this.productID = productID;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.status = status;
+        this.supplier = supplier;
     }
+
+    public int getProductID() { return productID; }
+    public String getProductName() { return name; }
+    public int getQuantity() { return quantity; }
+    public double getPrice() { return price; }
+    public String getStatus() { return status; }
+    public Supplier getSupplier() { return supplier; }
+}
