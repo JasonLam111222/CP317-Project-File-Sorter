506563273c167fdc2d62dffe149cc19c312ddcf7
JasonLam111222 <157567859+JasonLam111222@users.noreply.github.com>
2025-03-17 20:52:06 -0400

    Create Product.java

---
+++ src/Product.java
@@ -0,0 +1,10 @@
+class Product {
+    private String productId;
+    private String name;
+    private double price;
+
+    public Product(String productId, String name, double price) {
+        setProductId(productId);
+        setName(name);
+        setPrice(price);
+    }
