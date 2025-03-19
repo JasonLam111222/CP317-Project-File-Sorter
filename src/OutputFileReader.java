e19d004c4270f408cd655ae823108bc728d09fd6
Jason Lam <157567859+JasonLam111222@users.noreply.github.com>
2025-03-18 20:15:29 -0400

    Update OutputFileReader.java

--- src/OutputFileReader.java
+++ src/OutputFileReader.java
@@ -1,3 +1,26 @@
 import java.io.*;
 import java.util.*;
 import com.inventory.*;
+
+class OutputFileWriter extends FileHandler {
+
+    @Override
+    public void writeData(String filePath, List<Product> products) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            for (Product product : products) {
+                bw.write(product.getProductID() + ", " +
+                         product.getProductName() + ", " +
+                         product.getQuantity() + ", " +
+                         product.getPrice() + ", " +
+                         product.getStatus() + ", " +
+                         product.getSupplier().getName());
+                bw.newLine();
+            }
+        }
+    }
+
+    @Override
+    public List<Product> readData(String filePath) {
+        throw new UnsupportedOperationException("OutputFileWriter cannot read data.");
+    }
+}
